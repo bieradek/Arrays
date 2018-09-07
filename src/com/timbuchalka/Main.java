b877de190085d8083a38ba8db1347c1b59e1d5b6
Radosław Biedrzycki <radoslaw.biedrzycki@syncron.com>
2018-09-07 19:03:31 +0200

    Introducing 2 ways of initializing Arrays

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,11 +3,19 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        int[] myIntArray = new int[10];
-        myIntArray[5] = 50;
-        double[] myDoubleArray = new double[10];
-        System.out.println(myIntArray[5]);
+//        int[] myIntArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] myIntArray = new int[25];
+        for (int i = 0; i < myIntArray.length; i++) {
+            myIntArray[i] = i * 10;
+        }
+        for (int i = 0; i < myIntArray.length; i++) {
+            System.out.println("Element " + i + " value is " + myIntArray[i]);
+        }
+        System.out.println(myIntArray[0]);
+        System.out.println(myIntArray[1]);
+        System.out.println(myIntArray[2]);
+        System.out.println(myIntArray[3]);
+        System.out.println(myIntArray[4]);
 
     }
 }
