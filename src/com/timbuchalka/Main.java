a6873b6bc3f9ef1a4aa512d90e5df8f600099c9f
bieradek <biedrzycki.radek@gmail.com>
2018-09-12 00:39:44 +0200

    Uploading some code

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -12,23 +12,17 @@ public class Main {
 //        System.out.println(slownik[3]);
 //        char[] tablicaZnakow = {'s', 'd', 'e', 'g', 'a', 'i'};
 //        double[] ;
-//
-//
+
+
 //        int[] myIntegers = getIntegers(5); // the array myIntegers will contain VALUES array
 //        for (int i = 0; i < myIntegers.length; i++) {
 //            System.out.println("Element " + i + " typed value was " + myIntegers[i]);
 //        }
-        getAverage(10);
+//*******************CHALLENGE*******************
+        getIntegers(5) =
 
-    }
 
-    public static int[] getIntegers(int number) { // declaring a method which will return an ARRAY using NUMBER
-        System.out.println("Enter " + number + " integer values.\r"); // asking for input in the next line
-        int[] values = new int[number]; // declaring the length of the TEMPORARY array, the length is based on NUMBER
-        for (int i = 0; i < values.length; i++) {
-            values[i] = scanner.nextInt();
-        }
-        return values; // [1,2,3,4,5,6,7,8,9,10]
+
     }
 
     public static double getAverage(int number) {
@@ -41,5 +35,28 @@ public class Main {
         }
         System.out.println(sum / values.length);
         return sum / values.length;
+    }
+
+    public static double getAverageTimsCode(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return (double) sum / (double) array.length;
+    }
+
+    public static int[] sortDescending(int[] array){
+
+    }
+
+
+
+    public static int[] getIntegers(int number) { // declaring a method which will return an ARRAY using NUMBER
+        System.out.println("Enter " + number + " integer values.\r"); // asking for input in the next line
+        int[] values = new int[number]; // declaring the length of the TEMPORARY array, the length is based on NUMBER
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+        return values; // [1,2,3,4,5,6,7,8,9,10]
     }
 }
